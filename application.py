d08cc8788ddcfb5a0545b0ae8bccb8115b85a1e1
Sagar Kangutkar <sagark9040@gmail.com>
2019-07-28 20:40:28 -0700

    working samples of commute analyzer

--- application.py
+++ application.py
@@ -1,94 +1,168 @@
-import os
+import time
+import pyodbc
 import requests
 import json
-import time
-import xlsxwriter
 import sys
+import logging
+from datetime import datetime
+from pytz import timezone
 from flask import Flask
 
-my_app = Flask(__name__)
+app = Flask(__name__)
 
-@my_app.route("/")
-def hello():
-    return "Hello Flask, on Azure App Service for Linux"
+server = 'commuteanalyzer.database.windows.net'
+database = 'commuteanalyzer'
+username = 'sagar.kangutkar'
+password = 'BuffaloFlames14'
+driver = '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect(
+    'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+cursor = cnxn.cursor()
+
+homes = {"1117 NW 56th St, Seattle, WA 98107", "806-NW-63rd-St-98107"}
+offices = {"3009 157th Pl NE, Redmond, WA 98052"}
+
+fmt = "%Y-%m-%d %H:%M:%S"
+now_pacific = datetime.now(timezone('US/Pacific'))
+cur_time = now_pacific.strftime(fmt)
+is_weekday = True if now_pacific.weekday() < 5 else False
+
+
+def init():
+    cursor.execute("SELECT count(*) FROM CommuteAnalyzer")
+    all_data = 'number of rows -->'
+    all_data += str(cursor.fetchone()[0])
+    return all_data
+
+
+@app.route("/")
+def run():
+    logging.basicConfig(level=logging.DEBUG)
+    all_data = init()
+
+    # only concerned window. Else break
+    cur_hour = now_pacific.hour
+    if is_weekday and ((cur_hour >= 7 and cur_hour < 11) or (cur_hour >= 15 and cur_hour < 19)):
+        run_all_combinations()
+
+        cursor.execute("SELECT * FROM CommuteAnalyzer")
+        logging.debug("Inside Commute Window --> " + str(cursor.fetchone()[0]))
+
+        for row in cursor.fetchall():
+            all_data += str(row)
+            all_data += '\n'
+
+    else:
+        all_data = "outside the daily commute window " + all_data
+        logging.debug(all_data)
+    return all_data
+
+
+@app.route("/force")
+def force_run():
+    all_data = init()
+
+    # run always on force
+    run_all_combinations()
+
+    cursor.execute("SELECT * FROM CommuteAnalyzer")
+    for row in cursor.fetchall():
+        all_data += str(row)
+        all_data += '\n\n'
+
+    return all_data
+
+
+def temp():
+    print(now_pacific.hour)
+
+
+# saa = "->"
+# cursor.execute("insert into CommuteAnalyzer values('" + saa + "', 'desti', 1, 2, 3, 4, 'maps link', " + time.strftime("%Y%m%d-%H%M") +")")
+# cnxn.commit()
+
+# cursor.execute("SELECT count(*) FROM CommuteAnalyzer")
+# print ("-->" + str(cursor.fetchone()[0]))
+# cursor.execute("SELECT * FROM CommuteAnalyzer")
+
+# #print ("cur time ===> " + cur_time)
+
+# for row in cursor.fetchall():
+#     print (row)
+
+# return "Hello World!"
 
 def get_shortest_duration(origin, destination, avoid_tolls):
+    request_string = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + origin \
+                     + '&destination=' + destination \
+                     + '&sensor=false&mode=driving&alternatives=true' + \
+                     '&key=AIzaSyAWIzWB1NsK6TKNCMlqLdbmhiq4hKm-Szk&departure_time=now'
 
-	request_string = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + origin \
-					 + '&destination=' + destination \
-					 + '&sensor=false&mode=driving&alternatives=true' + \
-					 '&key=AIzaSyAWIzWB1NsK6TKNCMlqLdbmhiq4hKm-Szk&departure_time=now'
+    if avoid_tolls:
+        request_string += "&avoid=tolls"
 
-	if avoid_tolls:
-		request_string += "&avoid=tolls"
+    # print (request_string)
 
-	r = requests.get(request_string)
+    r = requests.get(request_string)
 
+    resp = json.loads(r.text)
+    # print(resp['routes'])
+    routes = resp['routes']
+    shortest_dur = sys.maxsize
+    shortest_dist = sys.float_info.max
 
-	resp = json.loads(r.text)
-	# print(resp['routes'])
-	routes = resp['routes']
-	shortest_dur = sys.maxsize
-	shortest_dist = sys.float_info.max
+    for key in range(0, len(routes)):
+        value = routes[key]
+        legs = value['legs']
+        duration = 0
+        distance = 0
 
-	for key in range(0, len(routes)):
-		value = routes[key]
-		legs = value['legs']
-		duration = 0
-		distance = 0
+        for l in range(0, len(legs)):
+            leg = legs[l]
+            duration += int(leg['duration_in_traffic']['text'].split(" ", 1)[0])
+            distance += float(leg['distance']['text'].split(" ", 1)[0])
 
-		for l in range(0, len(legs)):
-			leg = legs[l]
-			duration += int(leg['duration_in_traffic']['text'].split(" ", 1)[0])
-			distance += float(leg['distance']['text'].split(" ", 1)[0])
+        # print duration
+        # print distance
 
-		# print duration
-		# print distance
+        shortest_dur = min(shortest_dur, duration)
+        shortest_dist = min(shortest_dist, distance)
 
-		shortest_dur = min(shortest_dur, duration)
-		shortest_dist = min(shortest_dist, distance)
-
-	print("miles: " + str(shortest_dist))
-	print("mins: " + str(shortest_dur))
-	return shortest_dur, shortest_dist
+    # print("miles: " + str(shortest_dist))
+    # print("mins: " + str(shortest_dur))
+    return shortest_dur, shortest_dist
 
 
 def run_all_combinations():
-	cur_time = time.strftime("%Y%m%d-%H%M")
-	print (cur_time)
+    # afternoon
+    if now_pacific.hour > 12:
+        origins = offices
+        destinations = homes
+    else:
+        # mornings
+        origins = homes
+        destinations = offices
 
-	workbook = xlsxwriter.Workbook(cur_time + '.xlsx')
-	worksheet = workbook.add_worksheet()
-	row = 0
-	worksheet.write(row, 0, 'Origin')
-	worksheet.write(row, 1, 'Destination')
-	worksheet.write(row, 2, 'without Tolls (Mins)')
-	worksheet.write(row, 3, 'WITH Tolls (Mins)')
-	worksheet.write(row, 4, 'without Tolls (Miles)')
-	worksheet.write(row, 5, 'WITH Tolls (Miles)')
-	row += 1
+    for origin in origins:
+        for destination in destinations:
+            map_url = generate_map_url(origin, destination)
+            # print (origin + " ----> " + destination)
+            avoid_tolls = get_shortest_duration(origin, destination, True)
+            with_tolls = get_shortest_duration(origin, destination, False)
+            query = "insert into CommuteAnalyzer values('" + origin + "','" + destination + "', " + str(
+                avoid_tolls[0]) + ", " + str(with_tolls[0]) + ", " + str(avoid_tolls[1]) + ", " + str(
+                with_tolls[1]) + ", '" + map_url + "'," + "'" + cur_time + "')"
+            cursor.execute(query)
+            cnxn.commit()
 
-	origins = {"1117 NW 56th St, Seattle, WA 98107", "806-NW-63rd-St-98107"}
-	destination = "3009 157th Pl NE, Redmond, WA 98052"
 
-	for origin in origins:
-		map_url = generate_map_url(origin, destination)
-
-		avoid_tolls = get_shortest_duration(origin, destination, True)
-		with_tolls = get_shortest_duration(origin, destination, False)
-		worksheet.write(row, 0, origin)
-		worksheet.write(row, 1, destination)
-		worksheet.write(row, 2, avoid_tolls[0])
-		worksheet.write(row, 3, with_tolls[0])
-		worksheet.write(row, 4, avoid_tolls[1])
-		worksheet.write(row, 5, with_tolls[1])
-		worksheet.write(row, 6, map_url)
-
-		row += 1
-
-	workbook.close()
+# print ("................................................")
+# cursor.execute("SELECT * FROM CommuteAnalyzer")
+# for row in cursor.fetchall():
+#    print (row)
 
 def generate_map_url(origin, destination):
-	return "https://www.google.com/maps/dir/" + origin + "/" + destination
+    return "https://www.google.com/maps/dir/" + origin + "/" + destination
 
-#run_all_combinations()
+# print(force_run())
+# temp()
